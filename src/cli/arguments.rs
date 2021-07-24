456b89c2c67e2346a1b10c925367123d17138090
Takasakiii <lucasmc2709@live.com>
2021-07-24 17:39:32 -0300

    arguments base 2

---
+++ src/cli/arguments.rs
@@ -0,0 +1,49 @@
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(
+    name = "Ena-Code",
+    about = "A simple profile switcher for Visual Studio Code\n\nStill in beta."
+)]
+#[clap(version = env!("CARGO_PKG_VERSION"), author = "Takasakiii <lucasmc2709@live.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+pub struct LaunchOptions {
+    #[clap(subcommand)]
+    pub sub: SubCommands,
+}
+
+#[derive(Clap)]
+pub enum SubCommands {
+    #[clap(about = "Incia o Visual Studio Code com um profile.")]
+    Run(Run),
+    #[clap(about = "Lista os profiles")]
+    Ls(List),
+    #[clap(about = "Remove um profile")]
+    Rm,
+}
+
+#[derive(Clap)]
+pub struct Run {
+    #[clap(default_value = "Default", about = "Profile name.")]
+    pub profile: String,
+    #[clap(about = "Workflow folder. ")]
+    pub path: Option<String>,
+    #[clap(
+        short,
+        long,
+        about = "Changes the 'derive' of the 'Default' profile to another one."
+    )]
+    pub base_derive: Option<String>,
+}
+
+#[derive(Clap)]
+pub struct List {
+    #[clap(long)]
+    a: bool,
+}
+
+impl LaunchOptions {
+    pub fn build() -> Self {
+        LaunchOptions::parse()
+    }
+}
