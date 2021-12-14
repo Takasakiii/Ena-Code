341effdefa0b8ff54cdc50fedc08e6b7ad9a1236
Takasakiii <lucasmc2709@live.com>
2021-12-14 04:24:50 -0300

    Fix bug panic quando profiles está vazia

--- src/manager/list.rs
+++ src/manager/list.rs
@@ -4,23 +4,25 @@ use ecode_core::configs::dirs_and_files;
 
 pub fn list_profiles() {
     let profile_path = dirs_and_files::get_profiles_folder_path();
-    let mut itens = fs::read_dir(profile_path)
-        .unwrap()
-        .map(|profile| {
-            if let Ok(profile) = profile {
-                profile.file_name().to_str().unwrap().to_string()
-            } else {
-                "Sem Permissão".to_owned()
-            }
-        })
-        .collect::<Vec<_>>();
+    let itens = fs::read_dir(profile_path);
 
-    itens.sort();
+    if let Ok(itens) = itens {
+        let mut itens = itens
+            .map(|profile| {
+                if let Ok(profile) = profile {
+                    profile.file_name().to_str().unwrap().to_string()
+                } else {
+                    "Sem Permissão".to_owned()
+                }
+            })
+            .collect::<Vec<_>>();
+        itens.sort();
 
-    let print_itens = itens
-        .iter()
-        .filter(|item| *item != "Sem Permissão" && *item != "Default")
-        .fold(String::new(), |acc, new| format!("{}{} ", acc, new));
+        let print_itens = itens
+            .iter()
+            .filter(|item| *item != "Sem Permissão" && *item != "Default")
+            .fold(String::new(), |acc, new| format!("{}{} ", acc, new));
 
-    println!("{}", print_itens);
+        println!("{}", print_itens);
+    }
 }
