49b5ff579a4b6e5b5e3c35c681773a38319d3bad
Takasakiii <lucasmc2709@live.com>
2022-04-30 15:44:08 -0300

    fix: launcher code, removing unwraps

--- src/ecode/launcher.rs
+++ src/ecode/launcher.rs
@@ -15,64 +15,63 @@ pub fn launch(args: &LaunchOptions, config: &Config) {
     let extension_folder = extension_folder.to_str();
     let configs_folder = configs_folder.to_str();
 
-    if extension_folder.is_some() && configs_folder.is_some() {
-        match &args.base_derive {
-            None => {
-                if !check_profile_exists(&args.profile) {
-                    let user_response = scanln::scanln!(
-                        "The profile {} doesn't exist. Do you want to create it? [y/N]: ",
-                        &args.profile
-                    );
+    if let Some(extension_folder) = extension_folder {
+        if let Some(configs_folder) = configs_folder {
+            match &args.base_derive {
+                None => {
+                    if !check_profile_exists(&args.profile) {
+                        let user_response = scanln::scanln!(
+                            "The profile {} doesn't exist. Do you want to create it? [y/N]: ",
+                            &args.profile
+                        );
 
-                    if user_response.ne("y") {
-                        return;
+                        if user_response.ne("y") {
+                            return;
+                        }
+                    }
+                    copy_profile(&args.profile, &config.create_new_profile_from)
+                }
+                Some(profile_in_args) => copy_profile(&args.profile, profile_in_args),
+            }
+
+            let path_workflow = match &args.path {
+                Some(path) => &path[..],
+                None => {
+                    if config.default_current_folder {
+                        "."
+                    } else {
+                        ""
                     }
                 }
-                copy_profile(&args.profile, &config.create_new_profile_from)
-            }
-            Some(profile_in_args) => copy_profile(&args.profile, profile_in_args),
-        }
+            };
 
-        let path_workflow = match &args.path {
-            Some(path) => &path[..],
-            None => {
-                if config.default_current_folder {
-                    "."
-                } else {
-                    ""
+            if args.verbose {
+                println!(
+                    "DirsFinal: {{ex: {}, cf: {}, pl: {}}}",
+                    extension_folder, configs_folder, path_workflow
+                );
+            }
+
+            let cmd_exec = Command::new(&config.vs_code_path[..])
+                .arg(path_workflow)
+                .arg("--extensions-dir")
+                .arg(extension_folder)
+                .arg("--user-data-dir")
+                .arg(configs_folder)
+                .output();
+
+            match cmd_exec {
+                Err(why) => println!("Error starting VSCode proccess: {:?}", why),
+                Ok(out) => {
+                    if args.verbose {
+                        println!("{:?}", out)
+                    }
                 }
             }
-        };
-
-        if args.verbose {
-            println!(
-                "DirsFinal: {{ex: {}, cf: {}, pl: {}}}",
-                extension_folder.unwrap(),
-                configs_folder.unwrap(),
-                path_workflow
-            );
-        }
-
-        let cmd_exec = Command::new(&config.vs_code_path[..])
-            .arg(path_workflow)
-            .arg("--extensions-dir")
-            .arg(extension_folder.unwrap())
-            .arg("--user-data-dir")
-            .arg(configs_folder.unwrap())
-            .output();
-
-        match cmd_exec {
-            Err(why) => println!(
-                "Error starting VSCode proccess: {:?}",
-                why
-            ),
-            Ok(out) => {
-                if args.verbose {
-                    println!("{:?}", out)
-                }
-            }
+        } else {
+            println!("Error building VSCode launch: extensions folder is null.")
         }
     } else {
-        println!("Error building VSCode launch: extensions folder or configs folder is null.")
+        println!("Error building VSCode launch: configs folder is null.")
     }
 }
